d6484506e9251a49f8d214b2db4c762891408044
wyattsweet <wyattsweet@gmail.com>
2017-02-10 19:10:56 -0800

    insertion sort

--- algorithms/javavscript/insertion_sort.js
+++ algorithms/javavscript/insertion_sort.js
@@ -4,49 +4,24 @@
 // Each element in the unsorted list gets compared to each element in the sorted list from left to right
 // If the unsorted element is lower than the sorted element the sorted element moves one place to the right
 
-function insert(unsorted_arr) {
-  var sorted_arr = [];
-  for (var i = 0; i < unsorted_arr.length; i++) {
+var arr = [1000, 4, 2, 13, 3, 2, 1];
 
-    if (sorted_arr.length == 0) {
-      // 1. check if array is empty, if yes insert value and move on
-      //sorted_arr.push(unsorted_arr[i]);
+for (var i = 1; i < arr.length; i++) { // 3 
+  var current = arr[i];
 
-      sorted_arr.push(i);
+  for (var n = 1; n < arr.length; n++) {
+       prev = arr[i - n];
 
-    } else {
-      console.log('b is', b);
+    if (prev > current) {
+      arr[i - n] = arr[(i - n) + 1];
+      arr[(i - n) + 1] = prev;
+    }
 
-      for (var b = sorted_arr.length - 1; b == 0; b--) {
-        sorted_arr.push(i);
-  
-//        current_val = sorted_arr[b];
-//        prev_val = sorted_arr[b - 1];
-//
-//        // if current_val is the first element in the array
-//        if (unsorted_arr[i] < current_val && prev_val == undefined) {
-//          sorted_arr.unshift(unsorted_arr[i]);
-//          break;
-//        }
-//
-//        // if unsorted value is smaller than current_val and greater than prev_val
-//        if (unsorted_arr[i] < current_val && unsorted_arr[i] > prev_val) {
-//          sorted_arr[b + 1] = sorted_arr[b];
-//          sorted_arr[b] = unsorted_arr[i];
-//          break;
-//        }
-//  
-//        // If unsorted value is greated than sorted value
-//        if (unsorted_arr[i] > current_val ) {
-//          //  a. if so push to end of array
-//          sorted_arr.push(unsorted_arr[i]);
-//          break;
-//        }
-      }
+    if (prev < current) {
+      break;
     }
   }
 
-  console.log(sorted_arr);
 }
 
-insert([9, 7, 3, 2, 3, 23, 4]);
+console.log(arr);
