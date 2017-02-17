b30bad05cfb9d11d122d56930618a794cb289e6b
wyattsweet <wyattsweet@gmail.com>
2017-02-16 18:22:56 -0800

    update

---
+++ javascript/types.js
@@ -0,0 +1,21 @@
+// types in JavaScript
+
+// JavaScript is a loosely typed or dynamic language, which means you don't have to declare the
+// type of a variable ahead of time. This means you can have the same variable as different types.
+
+var foo = 42; //foo is now a Number
+var foo = 'bar'; // foo is now a String
+var foo = true; // foo is now a Boolean
+
+// The latest ECMAScript standard defines seven data types:
+//
+// Six data types are primitive
+//
+// Boolean
+// Null
+// Undefined
+// Number
+// String
+// Symbol (New is ES6)
+// 
+// and Object
