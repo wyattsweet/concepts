1a873ec5b67f9cc4d1622e8ab424db1975c769b1
wyattsweet <wyattsweet@gmail.com>
2017-02-22 10:29:49 -0800

    new stuff

---
+++ java/Lynda.com Up and Running with Java - notes and code/02_Java_Basics/challenge/SimpleCalculation.java
@@ -0,0 +1,37 @@
+// find the area of a house that needs to be painted
+// 
+import java.util.Scanner;
+public class SimpleCalculation {
+
+  public static void main(String[] args) {
+    
+    int house_length, house_height, house_depth, door_count, door_width, door_height, win_count, win_width, win_height;
+
+    // ask user for area of the sides of the house
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the length of your house: ");
+    house_length = in.nextInt();
+
+    System.out.println("Enter the height of your house: ");
+    house_height = in.nextInt();
+
+    System.out.println("Enter the depth of your house: ");
+    house_depth = in.nextInt();
+
+    System.out.println("How many doors are there in your house: ");
+    door_count = in.nextInt();
+
+    System.out.println("enter door width: ");
+    door_width = in.nextInt();
+
+    System.out.println("enter door length: ");
+    door_height = in.nextInt();
+
+    System.out.println("How many windows are there: ");
+    win_count = in.nextInt();
+
+    // not finished, get the idea 
+
+  }
+
+}
