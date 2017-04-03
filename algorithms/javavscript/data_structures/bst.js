e7f557b142a51f861ce86f64f37495d268c2573b
wyattsweet <wyattsweet@gmail.com>
2017-04-02 18:04:00 -0700

    update

--- algorithms/javavscript/data_structures/bst.js
+++ algorithms/javavscript/data_structures/bst.js
@@ -10,12 +10,24 @@ class Tree {
   add(value) {
     const node = new Node(value);
     if (!this.root) {
-      this.root = value;
+      this.root = node;
     } else {
       // iterative approach
       let next = true;
+      let thisNode = this.root;
+      debugger;
       while(next) {
-        if 
+        if (value < thisNode.value && thisNode.left === null) {
+          thisNode.left = node
+          next = false;
+        } else if (value > thisNode.value && thisNode.right === null) {
+          thisNode.right = node;
+          next = false;
+        } else if (value < thisNode.value) {
+          thisNode = thisNode.left;
+        } else {
+          thisNode = thisNode.right;
+        }
       }
     }
   }
@@ -28,3 +40,12 @@ class Node {
     this.right = right;
   }
 }
+
+const tree = new Tree();
+tree.add(4);
+tree.add(6);
+tree.add(3);
+tree.add(7);
+tree.add(2);
+tree.add(31);
+console.log(tree);
